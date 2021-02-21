6b9609c85e5e05783fd56bc4adc47eedd2f0b498
Hritobroto Sanyal <60534062+hritobroto@users.noreply.github.com>
2021-02-21 20:58:04 +0530

    Added exception handling

---
+++ joystick_ver2.py
@@ -0,0 +1,44 @@
+import inputs
+x=0
+while True:
+    if(len(inputs.devices.gamepads) == 0): #checks if the controller is connected or not
+        print("Please connect controller!")
+    else:
+        
+        try:
+            events = inputs.get_gamepad()  #gets events from the controller, i.e, button clicks and joystick movements
+            for event in events:
+                x=0
+                #print(event.ev_type, event.code, event.state)
+                #print(event.code,event.state)
+
+                #Speed control
+                if(event.code == 'BTN_SOUTH' and event.state == 1):
+                    print('A')
+                if(event.code == 'BTN_NORTH' and event.state == 1):
+                    print('Y')
+                if(event.code == 'BTN_WEST' and event.state == 1):
+                    print('X')
+                if(event.code == 'BTN_EAST' and event.state == 1):
+                    print('B')
+
+                #Direction Control
+                if(event.code == 'ABS_RY' and event.state > 0):
+                    print('UP')
+                if(event.code == 'ABS_RY' and event.state < 0):
+                    print('DOWN')
+                if(event.code == 'ABS_RX' and event.state > 0):
+                    print('RIGHT')
+                if(event.code == 'ABS_RX' and event.state < 0):
+                    print('LEFT')
+
+
+                #Start and Select buttons
+                if(event.code == 'BTN_START' and event.state == 1):
+                    print('SELECT')
+                if(event.code == 'BTN_SELECT' and event.state == 1):
+                    print('START')
+        except:
+                if(x==0):
+                    print("Please connect controller!")
+                    x+=1
